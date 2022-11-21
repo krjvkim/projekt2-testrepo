3eb4461b509b473beec9f2c89d59d9c29aed68f3
krjvkim <jakrwt00@hs-esslingen.de>
2022-11-21 14:36:12 +0100

    w

--- frontend/module.php
+++ frontend/module.php
@@ -8,7 +8,7 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Adminportal - Module</title>
-    <link rel="stylesheet" href="css/headerfooter.css">
+    <link rel="stylesheet" href="/css/headerfooter.css">
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <link rel="stylesheet" href="css/module.css">
 </head>
