089c8a3fe098e9a9fa96389ae569252b06235298
krjvkim <jakrwt00@hs-esslingen.de>
2022-11-21 10:17:48 +0100

    dd

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use("/static", express.static(path.resolve(__dirname, "frontend", "static"))
 app.get('/*', (req, res) => {
   res.sendFile(path.resolve(__dirname, "frontend", "webappstartseite.html"));
   res.sendFile(path.resolve(__dirname, "frontend", "css", "style.css"));
-  res.sendFile(path.resolve(__dirname, "frontend", "projektpartnerseite.html"));
+  res.sendFile(path.resolve(__dirname, "frontend", "projektpartner.html"));
 })
 
 
